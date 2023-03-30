b924721821dcd4e183af2b03410567ed05d5f9e2
Cash3mod3l <110199412+Cash3mod3l@users.noreply.github.com>
2023-03-30 17:49:09 +0300

    добавление функций

--- project/scripts.js
+++ project/scripts.js
@@ -3,41 +3,72 @@ let inputText = document.querySelector("#text");
 const button = document.querySelector("#btn");
 let key = 1;
 
-function Component() {
-	let newTextSpan = document.createElement("span");
-	let divComponent = document.createElement("div");
+function NewTask() {
+	const newTextSpan = document.createElement("span");
+	const divComponent = document.createElement("div");
+	const buttonBlock = document.createElement("div");
 
+	buttonBlock.classList.add("buttonBlock");
 	divComponent.classList.add("newComponent");
+	newTextSpan.classList.add("newTextSpan");
 
 	newTextSpan.innerText = inputText.value;
 	if (inputText.value === "") {
 		return;
 	}
+
 	inputText.value = "";
 
 	divComponent.appendChild(newTextSpan);
+	divComponent.appendChild(buttonBlock);
 	content.appendChild(divComponent);
 
-	let btnComponent = document.createElement("button");
-	let textButton = document.createElement("span");
-	const text = document.createTextNode("Удалить");
+	const deleteBtn = document.createElement("button");
+	const textButton = document.createElement("span");
+	const deleteText = document.createTextNode("Удалить");
 
-	btnComponent.classList.add("btnNewComponent");
+	deleteBtn.classList.add("btnNewComponent");
 	textButton.classList.add("spanText");
 
-	textButton.appendChild(text);
-	btnComponent.appendChild(textButton);
-	divComponent.appendChild(btnComponent);
+	textButton.appendChild(deleteText);
+	deleteBtn.appendChild(textButton);
+	buttonBlock.appendChild(deleteBtn);
 
-	btnComponent.onclick = function RemoveComponent() {
+	const redactionBtn = document.createElement("button");
+	const redactText = document.createTextNode("Редактировать");
+	const textButtonRedact = document.createElement("span");
+
+	textButtonRedact.classList.add("spanText");
+	redactionBtn.classList.add("btnNewComponent");
+
+	textButtonRedact.appendChild(redactText);
+	redactionBtn.appendChild(textButtonRedact);
+	buttonBlock.appendChild(redactionBtn);
+
+	const checkBox = document.createElement("input");
+
+	checkBox.type = "checkbox";
+	checkBox.classList.add("checkBox");
+
+	divComponent.appendChild(checkBox);
+
+	checkBox.onchange = function () {
+		if (checkBox.checked) {
+			divComponent.style.backgroundColor = "#31c731";
+			buttonBlock.style.visibility = "hidden";
+			divComponent.style.transition = "0.2s";
+		} else {
+			divComponent.style.backgroundColor = "#dad9d9";
+			buttonBlock.style.visibility = "visible";
+			divComponent.style.transition = "0.3s";
+		}
+	};
+
+	deleteBtn.onclick = function Remove() {
 		divComponent.remove();
-		btnComponent.remove();
-		textButton.remove();
-		text.remove();
-		newTextSpan.remove();
 	};
 }
 
 button.onclick = function () {
-	Component();
+	NewTask();
 };
